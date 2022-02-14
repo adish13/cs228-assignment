c566f32bb4b85a1ae396f909168562eedc43d944
adish13 <adirocks0013@gmail.com>
2022-02-14 16:31:40 +0530

    Final Repository

--- rush_hour.py
+++ rush_hour.py
@@ -1,5 +1,39 @@
+"""
+0: Vertical Car
+1: Horizontal Car
+2: Mine
+3: Red Car
+"""
+
+
 from z3 import *
 import sys
+
+def print_steps(m):
+	for time in range(timeout):
+		answer = []
+		for i in range(n):
+			for j in range(n):
+				val = is_true(m[P[i][j][0][time]]) ^ is_true(m[P[i][j][0][time+1]])
+				if val:
+					answer=(i, j, "vertical")
+		for i in range(n):
+			for j in range(n):
+				val = is_true(m[P[i][j][1][time]]) ^ is_true(m[P[i][j][1][time+1]])
+				if val:
+					answer=(i, j, "horizontal")
+		for i in range(n):
+			for j in range(n):
+				val = is_true(m[P[i][j][2][time]]) ^ is_true(m[P[i][j][2][time+1]])
+				if val:
+					answer=(i, j, "mine")
+		for i in range(n):
+			for j in range(n):
+				val = is_true(m[P[i][j][3][time]]) ^ is_true(m[P[i][j][3][time+1]])
+				if val:
+					answer=(i, j, "red")
+		print(f"{answer[0]},{answer[1]}")
+
 
 info = []
 with open(sys.argv[1]) as f:
@@ -8,175 +42,208 @@ with open(sys.argv[1]) as f:
 
 n = info[0][0]
 timeout = info[0][1]
-i0 = int(info[1][0])
+i0 = int(info[1][0]) # Initial row of the red car
 
-cars = len(info) - 1
-no_hcars = 0
-no_vcars = 0
-no_mine = 0
-for i in range(2,len(info)):
-    if info[i][0] == 0:
-        no_vcars += 1
-    if info[i][0] == 1:
-        no_hcars += 1
-    if info[i][0] == 0:
-        no_hcars = 0
-    else:
-        no_mine += 1
+cars = len(info) - 1 # Initial number of cars
 
-#timeout = 1
+P = [[[[Bool(f"P_{i}_{j}_{car_type}_{time}") for time in range(timeout + 1)] for car_type in range(4)] for j in range(n)] for i in range(n)]
 
-# defining the initial variables
-P = [[Bool ("P_{}_{}".format(i,j)) for i in range(n*n*4)] for j in range(timeout+1)]
-# P[(4*n*i)+(4*j)+ t ] means car of type t is in cell (i,j) 
-# t = 3 red car, rest all same as question
-
-array_indices = []
-
+positions = []
 Fs = []
-Fs += [ P[0][(4*n*info[1][0]) + (4*info[1][1]) + 3] ]
-array_indices.append((4*n*info[1][0]) + (4*info[1][1]) + 3)
 
-# Encode already filled positions
-for a in range(2,len(info)):
-    k = info[a][0]
-    if k >= 0 and k <= 2:
-        Fs += [P[0][(4*n*info[a][1]) + (4*info[a][2]) + k]]
-        array_indices.append((4*n*info[a][1]) + (4*info[a][2]) + k)
-#print(Fs)
+# Add conditions for the red car
+positions.append([info[1][0], info[1][1], 3])
+Fs.append(P[info[1][0]][info[1][1]][3][0])
 
-for x in range(n*n*4):
-    if x not in array_indices:
-        Fs += [Not(P[0][x])]
+for a in range(2, len(info)): # Iterate from after the red car
+	car_type = info[a][0]
+	if car_type >= 0 and car_type <= 2:
+		i = info[a][1]
+		j = info[a][2]
+		Fs += [P[info[a][1]][info[a][2]][car_type][0]]
+		positions.append([i, j, car_type])
 
-# Encode for i,j  \sum_k x_i_j_k = 1
-# each square must have exactly one entry
-for k in range(timeout+1):
-    for i in range(n):
-        for j in range(n):
-            Fs += [PbLe([(x,1) for x in P[k][4*n*i + 4*j: 4*n*i+4*j + 4: 1]],1)]
+# Encode positions that are not filled
+for i in range(n):
+	for j in range(n):
+		for car_type in range(4):
+			if [i,j,car_type] not in positions:
+				Fs.append(Not(P[i][j][car_type][0]))
 
 
-# horizontal car at (i,j) implies nothing else at (i,j+1)
-for k in range(timeout+1):
-    for i in range(n):
-        for j in range(n-1):
-            Fs += [Or(Not(P[k][4*n*i+4*j+1]), 
-            And( Not(P[k][4*n*i+4*(j+1)+0]),Not(P[k][4*n*i+4*(j+1)+1]),Not(P[k][4*n*i+4*(j+1)+2]),Not(P[k][4*n*i+4*(j+1)+3])) )]
+# Every square must have exactly one entry
+# \sum p[i][j][k][time] = 1 at for every time
+for time in range(timeout + 1):
+	for i in range(n):
+		for j in range(n):
+			temp = []
+			for car_type in range(4):
+				temp.append(P[i][j][car_type][time])
+			Fs.append(PbLe([(x,1) for x in temp], 1))
 
-# red car at (i,j) implies nothing else at (i,j+1)
-for k in range(timeout+1):
-    for i in range(n):
-        for j in range(n-1):
-            Fs += [Or(Not(P[k][4*n*i+4*j+3]),
-            And( Not(P[k][4*n*i+4*(j+1)+0]),Not(P[k][4*n*i+4*(j+1)+1]),Not(P[k][4*n*i+4*(j+1)+2]),Not(P[k][4*n*i+4*(j+1)+3])) )]
+# Horizontal car at (i, j) implies nothing else at (i, j+1)
+for time in range(timeout + 1):
+	for i in range(n):
+		for j in range(n-1):
+			Fs.append(
+			Or(Not(P[i][j][1][time]), Not(Or(P[i][j+1][0][time],P[i][j+1][1][time],P[i][j+1][2][time],P[i][j+1][3][time])))
+			)
 
-# vertical car at (i,j) implies nothing else car at (i+1,j)
-for k in range(timeout+1):
-    for i in range(n-1):
-        for j in range(n):
-            Fs += [Or(Not(P[k][4*n*i+4*j+0]),
-            And(Not(P[k][4*n*(i+1)+4*j+0]),Not(P[k][4*n*(i+1)+4*j+1]),Not(P[k][4*n*(i+1)+4*j+2]),Not(P[k][4*n*(i+1)+4*j+3]))  )    ]
+# print(Fs)
+# Red Car at (i, j) implies nothing else at (i, j+1)
+for time in range(timeout + 1):
+	for i in range(n):
+		for j in range(n -1):
+			Fs.append(
+			Or(Not(P[i][j][3][time]), Not(Or(P[i][j+1][0][time],P[i][j+1][1][time],P[i][j+1][2][time],P[i][j+1][3][time])))
+			)
 
-# No horizontal cars in last column 
-for k in range(timeout+1):
-    for i in range(n):
-        Fs += [Not(P[k][4*n*i+4*(n-1)+1])]
+# Vertical car at (i, j) implies nothing else at (i+1, j)
+for time in range(timeout + 1):
+	for i in range(n - 1):
+		for j in range(n):
+			Fs.append(
+			Or(Not(P[i][j][0][time]), Not(Or(P[i+1][j][0][time],P[i+1][j][1][time],P[i+1][j][2][time],P[i+1][j][3][time])))
+			)
 
-# No red cars in last column 
-for k in range(timeout+1):
-    for i in range(n):
-        Fs += [Not(P[k][4*n*i+4*(n-1)+3])]
+# No horizontal cars in last column
+for time in range(timeout + 1):
+	for i in range(n):
+		Fs.append(Not(P[i][n-1][1][time]))
 
-# No vertical cars in last row 
-for k in range(timeout+1):
-    for j in range(n):
-        Fs += [Not(P[k][4*n*(n-1)+4*j+0])]
+# No red cars in last column
+for time in range(timeout + 1):
+	for i in range(n):
+		Fs.append(Not(P[i][n-1][3][time]))
 
-# vertical car at (i,j) implies no horizontal car  and no red car at (i+1,j-1)
-for k in range(timeout+1):
-    for i in range(n-1):
-        for j in range(1,n):
-            Fs += [  Or(Not(P[k][4*n*i+4*j]),And(Not(P[k][4*n*(i+1)+4*(j-1)+1]),Not(P[k][4*n*(i+1)+4*(j-1)+3])))]
+# No vertical cars in last row
+for time in range(timeout + 1):
+	for j in range(n):
+		Fs.append(Not(P[n-1][j][0][time]))
 
-#for k in range(timeout+1):
-#    for j in range(n-1):
-#        for i in range(1,n):
-#            Fs += [  Or(Not(P[k][4*n*i+4*j]),And(Not(P[k][4*n*(i-1)+4*(j+1)+1]),Not(P[k][4*n*(i-1)+4*(j+1)+3])))]
+# Vertical car at (i, j) implies no horizontal car and no red car at (i+1, j-1)
+for time in range(timeout + 1):
+	for i in range(n - 1):
+		for j in range(1, n):
+			Fs += [  Or(Not(P[i][j][0][time]),And(Not(P[i+1][j-1][1][time]),Not(P[i+1][j-1][3][time])))]
+# The mine stays in one place
+for time in range(timeout):
+	for i in range(n):
+		for j in range(n):
+			Fs.append(Or(Not(P[i][j][2][time]), P[i][j][2][time + 1]))
+			Fs.append(Or(Not(P[i][j][2][time+1]), P[i][j][2][time]))
+"""
+Encoding the Moves Now
+"""
+for time in range(timeout):
+	moves = []
 
-# The mine stays at the same place
-for k in range(timeout):
-    for i in range(n):
-        for j in range(n):
-            Fs += [Or(Not(P[k][4*n*i+4*j+2]),P[k+1][4*n*i+4*j+2])]
-            Fs += [Or(P[k][4*n*i+4*j+2],Not(P[k+1][4*n*i+4*j+2]))]
+	# Vertical Movement
+	# Vertically Upwards
+	for i in range(1, n-1):
+		for j in range(n):
+			moves.append(And(Not(P[i][j][0][time+1]), P[i][j][0][time], P[i-1][j][0][time+1]))
+	
+	# Vertically Downwards
+	for i in range(n-2):
+		for j in range(n):
+			moves.append(And(Not(P[i][j][0][time+1]), P[i][j][0][time], P[i+1][j][0][time+1]))
+	
+	# Horizontal Movement (Horizontal Car)
+	# Horizontally leftwards
+	for i in range(n):
+		for j in range(1, n-1):
+			moves.append(And(Not(P[i][j][1][time+1]), P[i][j][1][time], P[i][j-1][1][time+1]))
 
-for k in range(timeout):
-    moves = []
-    # Vertical Moves
-    for i in range(n-2):  
-        for j in range(n):
-            #temp = If(P[k][4*n*i+4*j], And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i+1)+4*j]), Not(And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i+1)+4*j])))
-            temp = And(P[k][4*n*i+4*j], And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i+1)+4*j]))
-            moves.append(temp)
-    for i in range(1, n-1):  
-        for j in range(n):
-            # temp = If(P[k][4*n*i+4*j], And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i-1)+4*j]), Not(And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i-1)+4*j])))
-            temp = And(P[k][4*n*i+4*j], And(Not(P[k+1][4*n*i+4*j]), P[k+1][4*n*(i-1)+4*j]))
-            moves.append(temp)
+	# Horizontally rightwards
+	for i in range(n):
+		for j in range(n-2):
+			moves.append(And(Not(P[i][j][1][time+1]), P[i][j][1][time], P[i][j+1][1][time+1]))
 
-    # Horizontal Moves
-    for i in range(n):  
-        for j in range(n-2):
-            #temp = If(P[k][4*n*i+4*j+1], And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j+1)+1]), Not(And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j+1)+1])))
-            temp = And(P[k][4*n*i+4*j+1], And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j+1)+1]))
-            moves.append(temp)
+	
+	# Horizontal Movement (Red Car)
+	# Horizontally leftwards
+	for i in range(n):
+		for j in range(1, n-1):
+			moves.append(And(Not(P[i][j][3][time+1]), P[i][j][3][time], P[i][j-1][3][time+1]))
 
-    for i in range(n):
-        for j in range(1, n - 1):  
-            #temp = If(P[k][4*n*i+4*j+1], And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j-1)+1]), Not(And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j-1)+1])))
-            temp = And(P[k][4*n*i+4*j+1], And(Not(P[k+1][4*n*i+4*j+1]), P[k+1][4*n*i+4*(j-1)+1]))
-            moves.append(temp)
+	# Horizontally rightwards
+	for i in range(n):
+		for j in range(n-2):
+			moves.append(And(Not(P[i][j][3][time+1]), P[i][j][3][time], P[i][j+1][3][time+1]))
+	
+	Fs.append(PbEq([(x,1) for x in moves],1))
 
-    # For the red car
-    for i in range(n):
-        for j in range(n-2):
-            # temp = If(P[k][4*n*i+4*j+3], And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j+1)+3]), Not(And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j+1)+3])))
-            temp = And(P[k][4*n*i+4*j+3], And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j+1)+3]))
-            moves.append(temp)
+# For Horizontal
+for time in range(1, timeout + 1):
+	for i in range(n):
+		for j in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][1][time]), P[i][j-1][1][time-1], P[i][j][1][time-1], P[i][j+1][1][time-1]))
+		Fs.append(Or(Not(P[i][0][1][time]), P[i][1][1][time-1], P[i][0][1][time-1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[i][n-2][1][time]), P[i][n-3][1][time-1], P[i][n-2][1][time-1]))
 
-    for i in range(n):
-        for j in range(1, n - 1):  
-            #temp = If(P[k][4*n*i+4*j+3], And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j-1)+3]), Not(And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j-1)+3])))
-            temp = And(P[k][4*n*i+4*j+3], And(Not(P[k+1][4*n*i+4*j+3]), P[k+1][4*n*i+4*(j-1)+3]))
-            moves.append(temp)
-    Fs += [PbEq([(x, 1) for x in moves], 1)]
-    #temp = And(P[0][4*n*1+4*0+3], And(Not(P[0+1][4*n*1+4*0+3]), P[0+1][4*n*1+4*(0+1)+3]))
-    #print(Fs)
-
-
-#for k in range(timeout + 1):
-#	temp = [ ]
-#	for i in range(4 * n * n):
-#		temp.append(P[k][i])
-#	Fs.append(PbEq([(x,1) for x in temp], cars))
-
-for k in range(timeout):
-	temp = []
-	for i in range(4 * n * n):
-		temp.append(Xor(P[k][i], P[k + 1][i]))
-	Fs.append(PbEq([(x,1) for x in temp], 2))
+# For Red Car
+for time in range(1, timeout + 1):
+	for i in range(n):
+		for j in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][3][time]), P[i][j-1][3][time-1], P[i][j][3][time-1], P[i][j+1][3][time-1]))
+		Fs.append(Or(Not(P[i][0][3][time]), P[i][1][3][time-1], P[i][0][3][time-1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[i][n-2][3][time]), P[i][n-3][3][time-1], P[i][n-2][3][time-1]))
 
 
+for time in range(1, timeout + 1):
+	for j in range(n):
+		for i in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][0][time]), P[i-1][j][0][time-1], P[i][j][0][time-1], P[i+1][j][0][time-1]))
+		Fs.append(Or(Not(P[0][j][0][time]), P[1][j][0][time-1], P[0][j][0][time-1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[n-2][j][0][time]), P[n-3][j][0][time-1], P[n-2][j][0][time-1]))
 
-#Goal Clause
+# # Total number of cars is constant
+# for time in range(timeout+1):
+# 	temp = []
+# 	for i in range(n):
+# 		for j in range(n):
+# 			for car_type in range(4):
+# 				temp.append(P[i][j][car_type][time])
+# 	Fs.append(PbEq([(x,1) for x in temp], cars))
+
+
+# Vertical Car
+for time in range(timeout):
+	for j in range(n):
+		for i in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][0][time]), P[i+1][j][0][time+1], P[i][j][0][time+1], P[i-1][j][0][time+1]))
+		Fs.append(Or(Not(P[0][j][0][time]), P[0][j][0][time+1], P[1][j][0][time+1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[n-2][j][0][time]), P[n-3][j][0][time+1], P[n-2][j][0][time+1]))
+
+for time in range(timeout):
+	for i in range(n):
+		for j in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][1][time]), P[i][j+1][1][time+1], P[i][j][1][time+1], P[i][j-1][1][time+1]))
+		Fs.append(Or(Not(P[i][0][1][time]), P[i][0][1][time+1], P[i][1][1][time+1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[i][n-2][1][time]), P[i][n-3][1][time+1], P[i][n-2][1][time+1]))
+
+for time in range(timeout):
+	for i in range(n):
+		for j in range(1, n-2):
+			Fs.append(Or(Not(P[i][j][3][time]), P[i][j+1][3][time+1], P[i][j][3][time+1], P[i][j-1][3][time+1]))
+		Fs.append(Or(Not(P[i][0][3][time]), P[i][0][3][time+1], P[i][1][3][time+1]))
+		if (n >= 3):
+			Fs.append(Or(Not(P[i][n-2][3][time]), P[i][n-3][3][time+1], P[i][n-2][3][time+1]))
+
+
+
+# Goal Clause
 temp = []
-for k in range(timeout+1):
-    temp.append(P[k][4*n*i0+4*(n-2)+3])
+for time in range(timeout+1):
+	temp.append(P[i0][n-2][3][time])
 
-Fs += [PbGe([(x,1) for x in temp], 1)]
-
-
+Fs.append(PbGe([(x,1) for x in temp],1))
 
 s = Solver()
 s.add(Fs)
@@ -184,30 +251,8 @@ s.add(Fs)
 result = s.check()
 
 if result == sat:
-    # get satisfying model
-    m = s.model()
-
-    # print only if value is true
-    # print (sorted ([(d, m[d]) for d in m], key = lambda x: str(x[0]))
-
-    for i in range(timeout+1):
-        for j in range(n*n*4):
-            print(str(P[i][j])+str(m.eval(P[i][j])))
-
-    # xor_list = []
-    # for i in range(1,timeout+1):
-    #     xor = []
-    #     for j in range(n*n*4):
-    #         xor.append( is_true(m[P[i][j]])^ is_true(m[P[i-1][j]])) 
-    #     print(xor)
-    #     xor_list.append(xor)
-
-    # print(xor_list)
-    # y = False
-    # x = True
-    # print(x^y)
-    # print(is_true(m[P[1][35]]))
-    print("SAT")
+	m = s.model()
+	print_steps(m)
 
 else:
     print("UNSAT")
